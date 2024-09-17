12c51f49bc9700ab40b0e8008fc83bcfb8560500
meowmeowidoido <abs.haidarimain@gmail.com>
2024-09-17 14:09:53 -0400

    Update Player.cs
    Implemented player movement

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codice.CM.WorkspaceServer.DataStore.Configuration;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,70 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
-
+    public bool moveLeft=false;
+    public bool moveUp = false;
+    public bool moveRight = false;
+    public bool moveDown = false;
+    public float velocity = 0.03f;
+   
     void Update()
     {
+       float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveLeft =true;
+            PlayerMovement();
+            
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveUp = true;
+            PlayerMovement();
 
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveDown = true;
+            PlayerMovement();
+
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            moveRight = true;
+            PlayerMovement();
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            moveLeft = false;
+            moveUp = false;
+            moveDown = false;
+            moveRight = false;
+        }
     }
+    public void PlayerMovement()
+    {
+        if (moveLeft == true)
+        {
+            transform.position = new Vector3(transform.position.x - velocity, transform.position.y);
+        }
+        if (moveUp == true)
+        {
 
+            transform.position = new Vector3(transform.position.x, transform.position.y + velocity);
+        }
+        if(moveDown == true)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - velocity);
+
+        }
+        if (moveRight==true)
+        {
+            transform.position = new Vector3(transform.position.x + velocity, transform.position.y);
+
+        }
+    }
+    
 }
