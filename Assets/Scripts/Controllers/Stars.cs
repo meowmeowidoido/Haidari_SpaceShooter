ada87516b2fb3fad58a060fc622997fc493691d5
meowmeowidoido <abs.haidarimain@gmail.com>
2024-09-21 00:16:36 -0400

    attempt task4

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,38 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    public int p=0;
 
     // Update is called once per frame
     void Update()
     {
+        DrawConstellation();
+    }
+
+    public void DrawConstellation()
+    {
+       
+
+
+        foreach (Transform t in starTransforms)
+        {
+
+            Vector2 points;
+
+
+            if (p<starTransforms.Count)
+            {
+               
+               
+                 points = starTransforms[p].position;
+                
+                Debug.DrawLine(transform.position, points, Color.blue);
+                p++;
+            }
+           else {
+                p = 0;
+            }
+
+        }
     }
 }
